1ce44f8e491210368315d2cde4127b94558f6375
Matt Smith <mattrs290@gmail.com>
2020-08-23 10:45:40 -0400

    Initial Commit

---
+++ NewTanks/Source/NewTanks/NewTanksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWTANKS_API ANewTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
